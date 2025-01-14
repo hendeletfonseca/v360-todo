15a974ea86a38f84031cebe3f2cb036b3d886b40
Hendel Fonseca <hendeletfonseca@gmail.com>
2025-01-14 08:48:53 -0300

    Add Pages

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,54 +1,54 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show edit update destroy ]
+    before_action :set_todo, only: %i[ show edit update destroy ]
 
-  def index
-      @todos = if params[:text].present?
-                   Current.user.todos.where("title LIKE ?", "%#{params[:text]}%")
-      else
-                   Current.user.todos
-      end
-  end
+    def index
+        @todos = if params[:text].present?
+                     Current.user.todos.where("title LIKE ?", "%#{params[:text]}%")
+        else
+                     Current.user.todos
+        end
+    end
 
-  def show
-  end
+    def show
+    end
 
-  def new
-      @todo = Todo.new
-  end
+    def new
+        @todo = Todo.new
+    end
 
-  def create
-      @todo = Current.user.todos.build(todo_params)
+    def create
+        @todo = Current.user.todos.build(todo_params)
 
-      if @todo.save
-          redirect_to todos_path
-      else
-          render :new, status: :unprocessable_entity
-      end
-  end
+        if @todo.save
+            redirect_to todos_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
 
-  def edit
-  end
+    def edit
+    end
 
-  def update
-      if @todo.update(todo_params)
-          redirect_to todos_path
-      else
-          render :edit, status: :unprocessable_entity
-      end
-  end
+    def update
+        if @todo.update(todo_params)
+            redirect_to todos_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
 
-  def destroy
-      @todo.destroy
-      redirect_to todos_url
-  end
+    def destroy
+        @todo.destroy
+        redirect_to todos_url
+    end
 
-  private
+    private
 
-  def set_todo
-      @todo = Current.user.todos.find(params[:id])
-  end
+    def set_todo
+        @todo = Current.user.todos.find(params[:id])
+    end
 
-  def todo_params
-      params.expect(todo: [ :title, :description ])
-  end
+    def todo_params
+        params.expect(todo: [ :title, :description ])
+    end
 end
